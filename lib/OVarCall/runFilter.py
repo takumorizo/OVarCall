dc5f975d245ee87fc056e47767c05ffa16f3fcf9
森山卓也 <moriyamatakuya@moriyamatakuya-no-MacBook-Air-2.local>
2017-05-18 18:17:22 +0900

    check runFilter and usual run using the usual data sets

--- lib/OVarCall/runFilter.py
+++ lib/OVarCall/runFilter.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 from OVarCall.methods.ovarCall import OVarCall
 from OVarCall.utilOVar.exceptionUtil import *
-# from OVarCall.filter.pileUp import *
 from OVarCall.filter.pileUpFilter import PileUpFilter
 from OVarCall.utilOVar.bamDAO import BamDAO
 import logging
-import os
 import re
 import ConfigParser
 import vcf
@@ -45,70 +43,85 @@ def runOVarFilterANNOVAR(args):
     fasta = pysam.FastaFile(args.ref_fa)
     with tumorBamDao.openBam(), normalBamDao.openBam(), open(args.input,'r') as inputFile, open(args.output,'w') as outputFile:
         for line in inputFile:
-            line = line.replace('\n', '')
-            line = line.replace('\r', '')
-            lineCols = re.split('\t', line)
-            Chr, start, end, ref, alt = lineCols[0], lineCols[1], lineCols[2], lineCols[3], lineCols[4]
-            
             try:
-                start = int(start)
-                end = int(end)
-            except ValueError:
-                lineCols.append('OV+')
-                lineCols.append('OV-')
+                line = line.replace('\n', '')
+                line = line.replace('\r', '')
+                lineCols = re.split('\t', line)
+                Chr, start, end, ref, alt = lineCols[0], lineCols[1], lineCols[2], lineCols[3], lineCols[4]
+                
+                try:
+                    start = int(start)
+                    end = int(end)
+                except ValueError:
+                    lineCols.append('OV+')
+                    lineCols.append('OV-')
+                    outputString = '\t'.join(map(str, lineCols))
+                    outputString = outputString.replace('\n', '')
+                    outputString = outputString + '\n'
+                    outputFile.writelines(outputString)
+                    continue
+
+                TYPE = None
+                pos = int(start)
+                if ref != '-' and alt != '-':
+                    TYPE = 'M'
+                    ref = ref.upper()
+                    alt = alt.upper()
+                elif alt == '-':                
+                    TYPE = 'D'
+                    pos = start -1
+                    alt = ref.upper()
+                    ref = (fasta.fetch(Chr,pos-1,pos)).upper()
+                elif ref == '-':
+                    TYPE = 'I'
+                    alt = alt.upper()
+                    ref = (fasta.fetch(Chr,pos-1,pos)).upper()
+
+                headT, bodyT = tumorBamDao.getOverlapInformation(
+                    TYPE, Chr, pos, ref, alt)
+                headN, bodyN = normalBamDao.getOverlapInformation(
+                    TYPE, Chr, pos, ref, alt)
+
+                if (headT is None and bodyT is None) or (headN is None and bodyN is None):
+                    lineCols.append(-100) # overlap + 
+                    lineCols.append(-100) # overlap -
+                    logging.info("cannot get pileup info @ " + Chr + str(start) + "-" + str(end))
+                else :
+                    doFilter = args.filterPileup
+                    passFilter = not(doFilter)
+                    if doFilter:
+                        if filterTumor.filterOverlapPiled(headT, bodyT) and filterNormal.filterOverlapPiled(headN, bodyN):
+                            passFilter = True
+                    
+                    if passFilter:
+                        ansDic = ovarCall.doInference(TYPE, Chr, pos, ref, alt, headT, bodyT, bodyN)
+                        lineCols.append(ansDic["withOver"]) # overlap + 
+                        lineCols.append(ansDic["withoutOver"]) # overlap -
+                    else:
+                        lineCols.append(-100) # overlap + 
+                        lineCols.append(-100) # overlap -
+
                 outputString = '\t'.join(map(str, lineCols))
                 outputString = outputString.replace('\n', '')
                 outputString = outputString + '\n'
                 outputFile.writelines(outputString)
 
-            TYPE = None
-            pos = int(start)
-            if ref != '-' and alt != '-':
-                TYPE = 'M'
-                ref = ref.upper()
-                alt = alt.upper()
-            elif alt == '-':                
-                TYPE = 'D'
-                pos = start -1
-                alt = ref.upper()
-                ref = (fasta.fetch(Chr,pos-1,pos)).upper()
-            elif ref == '-':
-                TYPE = 'I'
-                alt = alt.upper()
-                ref = (fasta.fetch(Chr,pos-1,pos)).upper()
-
-            headT, bodyT = tumorBamDao.getOverlapInformation(
-                TYPE, Chr, pos, ref, alt)
-            headN, bodyN = normalBamDao.getOverlapInformation(
-                TYPE, Chr, pos, ref, alt)
-
-            if (headT is None and bodyT is None) or (headN is None and bodyN is None):
-                lineCols.append(-100) # overlap + 
-                lineCols.append(-100) # overlap -
-            else :
-                doFilter = args.filterPileup
-                passFilter = not(doFilter)
-                if doFilter:
-                    if filterTumor.filterOverlapPiled(headT, bodyT) and filterNormal.filterOverlapPiled(headN, bodyN):
-                        passFilter = True
-                
-                if passFilter:
-                    ansDic = ovarCall.doInference(TYPE, Chr, pos, ref, alt, headT, bodyT, bodyN)
-                    lineCols.append(ansDic["withOver"]) # overlap + 
-                    lineCols.append(ansDic["withoutOver"]) # overlap -
-                else:
-                    lineCols.append(-100) # overlap + 
-                    lineCols.append(-100) # overlap -
-
-
-            outputString = '\t'.join(map(str, lineCols))
-            outputString = outputString.replace('\n', '')
-            outputString = outputString + '\n'
-            outputFile.writelines(outputString)
-
+            except CliticalTypeException, e:
+                logging.error('clitical exception : ' + str(line))
+                raise e
+            except WarningTypeException, e:
+                logging.warning(
+                    'Because of warning type exception of ' + str(type(e)) + ' passed this line : ' + str(line))
+                continue
+            except Exception, e:
+                logging.error('type:' + str(type(e)))
+                logging.error('args:' + str(e.args))
+                logging.error('message:' + e.message)
+                logging.error('e:' + str(e))
+                logging.error('unexpected exception occurred. traceback here')
+                logging.error(traceback.format_exc())
+                continue
     fasta.close()
-
-
 
 
 
@@ -146,49 +159,62 @@ def runOVarFilterVCF(args):
         
         vcf_writer = vcf.Writer(open(args.output + ".vcf", 'w'), vcf_reader)
         for vcf_record in vcf_reader:
-            TYPE = None
-            Chr = str(vcf_record.CHROM)
-            pos = str(vcf_record.POS)
-            ref = str(vcf_record.REF).upper()
-            alt = str(vcf_record.ALT[0]).upper()
-            if len(ref) == 1 and len(alt) == 1:
-                TYPE = 'M'
-            elif len(ref) > len(alt):
-                TYPE = 'D'
-                alt = ref[1:]
-                ref = ref[0]
-            elif len(ref) < len(alt):
-                TYPE = 'I'
-                ref = ref[0]
-                alt = alt[1:]
+            try:
+                TYPE = None
+                Chr = str(vcf_record.CHROM)
+                pos = str(vcf_record.POS)
+                ref = str(vcf_record.REF).upper()
+                alt = str(vcf_record.ALT[0]).upper()
+                if len(ref) == 1 and len(alt) == 1:
+                    TYPE = 'M'
+                elif len(ref) > len(alt):
+                    TYPE = 'D'
+                    alt = ref[1:]
+                    ref = ref[0]
+                elif len(ref) < len(alt):
+                    TYPE = 'I'
+                    ref = ref[0]
+                    alt = alt[1:]
 
-            headT, bodyT = tumorBamDao.getOverlapInformation(TYPE, Chr, pos, ref, alt)
-            headN, bodyN = normalBamDao.getOverlapInformation(TYPE, Chr, pos, ref, alt)
+                headT, bodyT = tumorBamDao.getOverlapInformation(TYPE, Chr, pos, ref, alt)
+                headN, bodyN = normalBamDao.getOverlapInformation(TYPE, Chr, pos, ref, alt)
 
-            if (headT is None and bodyT is None) or (headN is None and bodyN is None):
-                vcf_record.INFO['OV+'] = -100
-                vcf_record.INFO['OV-'] = -100
-            else:
-                doFilter = args.filterPileup
-                passFilter = not(doFilter)
-                if doFilter:
-                    if filterTumor.filterOverlapPiled(headT, bodyT) and filterNormal.filterOverlapPiled(headN, bodyN):
-                        passFilter = True
-                
-                if passFilter:
-                    ansDic = ovarCall.doInference(TYPE, Chr, pos, ref, alt, headT, bodyT, bodyN)
-                    vcf_record.INFO['OV+'] = ansDic["withOver"]
-                    vcf_record.INFO['OV-'] = ansDic["withoutOver"]
-                else:
+                if (headT is None and bodyT is None) or (headN is None and bodyN is None):
                     vcf_record.INFO['OV+'] = -100
                     vcf_record.INFO['OV-'] = -100
+                    logging.info("cannot get pileup info @ " + Chr + str(start) + "-" + str(end))
+                else:
+                    doFilter = args.filterPileup
+                    passFilter = not(doFilter)
+                    if doFilter:
+                        if filterTumor.filterOverlapPiled(headT, bodyT) and filterNormal.filterOverlapPiled(headN, bodyN):
+                            passFilter = True
+                    
+                    if passFilter:
+                        ansDic = ovarCall.doInference(TYPE, Chr, pos, ref, alt, headT, bodyT, bodyN)
+                        vcf_record.INFO['OV+'] = ansDic["withOver"]
+                        vcf_record.INFO['OV-'] = ansDic["withoutOver"]
+                    else:
+                        vcf_record.INFO['OV+'] = -100
+                        vcf_record.INFO['OV-'] = -100
 
+                vcf_writer.write_record(vcf_record)
 
-            vcf_writer.write_record(vcf_record)
+            except CliticalTypeException, e:
+                logging.error('clitical exception : ' + str(line))
+                raise e
+            except WarningTypeException, e:
+                logging.warning(
+                    'Because of warning type exception of ' + str(type(e)) + ' passed this line : ' + str(line))
+                continue
+            except Exception, e:
+                logging.error('type:' + str(type(e)))
+                logging.error('args:' + str(e.args))
+                logging.error('message:' + e.message)
+                logging.error('e:' + str(e))
+                logging.error('unexpected exception occurred. traceback here')
+                logging.error(traceback.format_exc())
+                continue
 
         vcf_writer.close()
-
-
-
-
 
